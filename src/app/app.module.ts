72eb43d8247704724133242e966347b77840b18f
Abdur Rehman Khalid <abdur.r.khalid@gmail.com>
2019-09-06 19:34:53 +0500

    Made Service for Http Requests :speech_balloon:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MainWindowComponent } from "./main-window/main-window.component";
 
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, NavigationBarComponent, MainWindowComponent],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
